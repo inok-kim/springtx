74b92b54db9a1b64b60f1934916017a868ae77a5
iokim <iokim1220@gmail.com>
2022-10-16 22:59:53 +0900

    feat: 트랜잭션 AOP 주의 사항 - 초기화 시점

---
+++ src/test/java/hello/springtx/apply/InitTxTest.java
@@ -0,0 +1,51 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootTest
+public class InitTxTest {
+
+    @Autowired Hello hello;
+
+    @Test
+    void go() {
+        //초기화 코드는 스프링이 초기화 시점에 호출한다
+    }
+
+    @TestConfiguration
+    static class InitTxTestConfig {
+        @Bean
+        Hello hello() {
+            return new Hello();
+        }
+    }
+
+    @Slf4j
+    static class Hello {
+
+        @PostConstruct
+        @Transactional // PostContruct에서 Transactional 적용 안됨
+        public void initV1() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @PostContruct tx isActive={}", isActive);
+        }
+
+        @EventListener(ApplicationReadyEvent.class) // 스프링 컨테이너가 완전히 다 떳을때
+        @Transactional
+        public void initV2() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init ApplicationReadyEvent tx isActive={}", isActive);
+        }
+    }
+}
